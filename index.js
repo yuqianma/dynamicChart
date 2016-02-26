d780f7d80935e906f2b0d9c0645b0a2c8849ee57
Yuqian Ma <mayq0422@gmail.com>
2016-02-26 19:34:36 +0800

    remark and select

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/pebble', function (req, res) {
+  var data = {
+    x: req.query.x,
+    y: req.query.y,
+    z: req.query.z
+  }
+  console.log(data);
+  res.end();
+  io.emit('get data', data);
+});
+
+io.on('connection', function (socket) {
+  console.log('a connetion');
+});
+
+http.listen(3000, function () {
+  console.log('listening');
+});
